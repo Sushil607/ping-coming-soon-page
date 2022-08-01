53e177e3844b80f3e2fed6416bfe0a362aab0be3
Sushil607 <bodadesushil@gmail.com>
2022-08-01 16:55:07 +0530

    email validation done

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form");
+const email = document.getElementById("emailField");
+const error = document.getElementById("error");
+
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return false;
+  }
+  return true;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (email.value.trim() === "") {
+    email.style.cssText = "border: 2px solid var(--secondary-light-red)";
+    error.style.cssText = "display:block";
+    error.innerText = "Email cannot be empty.";
+  } else if (ValidateEmail(email.value)) {
+    email.style.cssText = "border: 2px solid var(--secondary-light-red)";
+    error.style.cssText = "display:block";
+    error.innerText = "Please provide a valid email address.";
+  } else {
+    email.style.cssText = "border: 2px solid var(--secondary-pale-blue)";
+    error.style.cssText = "display:none";
+  }
+});
